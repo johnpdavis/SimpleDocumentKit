3ec24263085a15479086bb20b8b9a866b2e0f6d0
John <davis.p.john@gmail.com>
2022-02-05 18:01:03 -0600

    Updates to unit tests and adds new items.

---
+++ Sources/SimpleDocumentKit/Documents/ComposableDocument/Mapping/ImageFolderMapItem.swift
@@ -0,0 +1,33 @@
+//
+//  ImageFolderMapItem.swift
+//  
+//
+//  Created by John Davis on 2/5/22.
+//
+
+import UIKit
+import UniformTypeIdentifiers
+
+public class ImageFolderMapItem: DynamicFolderMapItem {
+
+    var images: [UIImageFileMapItem] {
+        untrackedChildren.compactMap { $0 as? UIImageFileMapItem }
+    }
+    
+    public required init(filename: String) {
+        let imageFileTypes: [UTType : FileMapItemBase.Type] = [.png: UIImageFileMapItem.self,
+                                                               .jpeg: UIImageFileMapItem.self]
+        
+        super.init(filename: filename, supportedUntrackedTypes: imageFileTypes)
+    }
+    
+    public func setImages(_ images: [UIImage]) {
+        let items: [UIImageFileMapItem] = images.map { image in
+            let item = UIImageFileMapItem(filename: "\(UUID().uuidString).png")
+            item.setImage(image)
+            return item
+        }
+        
+        untrackedChildren = items
+    }
+}
