33409bf06c92a223be447e56da3ae2e19ec10366
John <davis.p.john@gmail.com>
2025-03-28 16:36:38 -0500

    remvoves useless debug

--- Sources/SimpleDocumentKit/DocumentManagers/QueryCoordinator/iCloudDocumentQueryCoordinator.swift
+++ Sources/SimpleDocumentKit/DocumentManagers/QueryCoordinator/iCloudDocumentQueryCoordinator.swift
@@ -66,8 +66,6 @@ public class iCloudDocumentQueryCoordinator: DocumentQueryCoordinator {
     @objc
     private func onMetaDataQuery(_ notification: Notification) {
         DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + 0.2, execute: { [weak self] in
-            
-            print(notification.userInfo)
             self?.processFilesAndSend()
         })
     }
